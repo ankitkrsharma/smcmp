10cdc92621e396f1eb757a188070742651e37069
ankitkrsharma <141256689+ankitkrsharma@users.noreply.github.com>
2025-12-13 18:37:19 +0530

    Initial push

---
+++ smcmp/src/main/java/com/secureoms/smcmp/SmcmpApplication.java
@@ -0,0 +1,13 @@
+package com.secureoms.smcmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcmpApplication.class, args);
+	}
+
+}
---
+++ smcmp/src/test/java/com/secureoms/smcmp/SmcmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureoms.smcmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmcmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
