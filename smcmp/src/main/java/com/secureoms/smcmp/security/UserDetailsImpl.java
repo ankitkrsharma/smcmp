6d70180707e9bf9aa6fd54e4407b381b0b71c229
ankitkrsharma <141256689+ankitkrsharma@users.noreply.github.com>
2025-12-21 21:02:09 +0530

    add

--- smcmp/src/main/java/com/secureoms/smcmp/security/UserDetailsImpl.java
+++ smcmp/src/main/java/com/secureoms/smcmp/security/UserDetailsImpl.java
@@ -1,54 +1,29 @@
-package com.secureoms.smcmp.security;
+// public class UserDetailsImpl implements UserDetails {
 
-import com.secureoms.smcmp.entity.User;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+//     private final User user;
 
-import java.util.Collection;
-import java.util.List;
+//     public UserDetailsImpl(User user) {
+//         this.user = user;
+//     }
 
-public class UserDetailsImpl implements UserDetails {
+//     // @Override
+//     // public Collection<? extends GrantedAuthority> getAuthorities() {
+//     //     return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+//     // }
 
-    private final User user;
+//     // @Override
+//     // public String getPassword() {
+//     //     return user.getPassword();
+//     // }
 
-    public UserDetailsImpl(User user) {
-        this.user = user;
-    }
+//     // @Override
+//     // public String getUsername() {
+//     //     return user.getUsername();
+//     // }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        // For now: single role
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return user.isEnabled();
-    }
-}
+// //     @Override public boolean isAccountNonExpired() { return true; }
+// //     @Override public boolean isAccountNonLocked() { return true; }
+// //     @Override public boolean isCredentialsNonExpired() { return true; }
+// //     @Override public boolean isEnabled() { return user.isEnabled(); }
+// // }
+fsEF
